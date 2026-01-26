fee3ea2cc9c79a3a8fd9cdf4d6240126e764d3a8
Mudit <mudit11545@gmail.com>
2026-01-26 16:43:26 +0530

    Populate songList with available tracks from songs

--- js/script.js
+++ js/script.js
@@ -1,9 +1,51 @@
 console.log("JavaScript beteeeeeeeeee")
 
-async function main(){
-    let a = await fetch("http://127.0.0.1:3000/Spotify%20Clone/songs/")
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:3000/songs/")
     let response = await a.text()
-    console.log(response)
+    let div = document.createElement("div")
+    div.innerHTML = response
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(decodeURIComponent(element.href).split("\\").pop());
+        }
+    }
+    return songs
+} 
+
+
+async function main() {
+    // getting list of all songs
+    let songs = await getSongs()
+    console.log(songs)
+
+
+    // showing all songs in playlist
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li>
+                            <img class="invert" src="img/music.svg">
+                            <div class="info">
+                                <div>${song.replaceAll("%20", " ")}</div>
+                                <div>Mudit </div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                                <img class="invert" src="img/play.svg">
+                            </div>
+                        </li>`
+    }
+
+    // Playing the first song
+    var audio = new Audio(songs[0]);
+    audio.play();     
+
+    audio.addEventListener("loadeddata", () => {
+        console.log(audio.duration,audio.currentSrc,audio.currentTime)
+    })
 }
 
 main()
