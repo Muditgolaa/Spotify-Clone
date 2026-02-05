b3e944a3a05cc45a8628f3938a73778d6e7683be
Mudit <mudit11545@gmail.com>
2026-02-05 21:25:56 +0530

    Added seekbar logic and improved audio controls

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 console.log("JavaScript beteeeeeeeeee")
 
+const currentSong = new Audio()
+
 async function getSongs() {
     let a = await fetch("http://127.0.0.1:3000/songs/")
     let response = await a.text()
@@ -16,12 +18,31 @@ async function getSongs() {
     return songs
 } 
 
+const playMusic = (track , pause = false) => {
+    currentSong.src = "/songs/" + track
+    if(!pause){
+        currentSong.play()
+        play.src = "img/pause.svg"
+    }
+    document.querySelector(".songinfo").innerHTML = track
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
+}
+
+function formatTime(seconds) {
+    if (isNaN(seconds) || seconds < 0) return "00:00";
+
+    const mins = Math.floor(seconds / 60);
+    const secs = Math.floor(seconds % 60);
+
+    return `${String(mins).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
+}
+
 
 async function main() {
     // getting list of all songs
     let songs = await getSongs()
-    console.log(songs)
 
+    playMusic(songs[0],true)
 
     // showing all songs in playlist
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
@@ -38,14 +59,41 @@ async function main() {
                             </div>
                         </li>`
     }
-
-    // Playing the first song
-    var audio = new Audio(songs[0]);
-    audio.play();     
-
-    audio.addEventListener("loadeddata", () => {
-        console.log(audio.duration,audio.currentSrc,audio.currentTime)
+ 
+    // attached a event listener to songList 
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
+        e.addEventListener("click",element=>{
+            console.log(e.querySelector(".info").firstElementChild.innerHTML)
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+        })
     })
+
+    // attached event listener to songbuttons
+    play.addEventListener("click",()=>{
+        if(currentSong.paused){
+            currentSong.play()
+            play.src = "img/pause.svg"
+        }
+        else{
+            currentSong.pause()
+            play.src = "img/play.svg"
+        } 
+    })
+
+    //listen for timeupdate event
+    currentSong.addEventListener("timeupdate",()=>{
+        document.querySelector(".songtime").innerHTML = `${formatTime(currentSong.currentTime)}/${formatTime(currentSong.duration)}` 
+        document.querySelector(".circle").style.left = (currentSong.currentTime/currentSong.duration) * 100 + "%"
+    })
+
+    //add an eventlistener to seekbar
+    document.querySelector(".seekbar").addEventListener("click",e=>{
+        console.log(e)
+        const percent = (e.offsetX/e.target.getBoundingClientRect().width)*100
+        document.querySelector(".circle").style.left = percent + "%"
+        currentSong.currentTime = ((currentSong.duration)*percent)/100
+    })
+
 }
 
 main()
